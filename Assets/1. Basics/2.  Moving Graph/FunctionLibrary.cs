2c7861009e5e01c826889f03e9a03680723107a3
luciPark <97658764+Luci-Park@users.noreply.github.com>
2022-11-18 17:30:07 +0900

    z added

--- Assets/1. Basics/2.  Moving Graph/FunctionLibrary.cs
+++ Assets/1. Basics/2.  Moving Graph/FunctionLibrary.cs
@@ -2,17 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FunctionLibrary : MonoBehaviour
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
 {
-    // Start is called before the first frame update
-    void Start()
+    public delegate float Function(float x, float z, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+
+    static Function[] functions = { Wave, MultiWave, Ripple};
+
+public static Function GetFunction(int index)
     {
-        
+        return functions[index];
     }
 
-    // Update is called once per frame
-    void Update()
+    public static Function GetFunction(FunctionName function)
     {
-        
+        return functions[(int) function];
+    }
+
+
+    public static float Wave(float x, float z, float t)
+    {
+        return Sin(PI * (x + t));
+    }
+
+    public static float MultiWave(float x, float z, float t)
+    {
+        float y = Sin(PI * (x + 0.5f * t));
+        y += 0.5f * Sin(2f * PI * (x + t));
+        return y *(2f / 3f);
+    }
+    public static float Ripple(float x, float z, float t)
+    {
+        float d = Abs(x);
+        float y = Sin(PI * (4f * d - t));
+        return y / (1f + 10f * d);
     }
 }
